26078f8153908636ff3d1cb88fe2bd6a597f8bed
Yoga Hanggara <yohang88@gmail.com>
2020-04-22 23:41:58 +0700

    sticky header
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-layout">
-    <div class="w-full bg-brand-green-dark text-white text-sm leading-6 font-medium p-3 text-center">
+    <div class="w-full bg-brand-green-dark text-white text-sm leading-6 font-medium p-3 text-center sticky top-0 z-20">
       <nuxt-link to="/">
         Rapid Test - PIKOBAR Jawa Barat
       </nuxt-link>

40d7e896064a02a1b08875b7c4831c59f1ec65ab
Yoga Hanggara <yohang88@gmail.com>
2020-05-13 02:10:11 +0700

    gender
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- pages/registration/personal.vue
+++ pages/registration/personal.vue
@@ -23,6 +23,33 @@
       </div>
 
       <div class="mt-6">
+        <validation-provider v-slot="{ errors }" name="Jenis Kelamin" rules="required">
+          <label class="block text-sm font-medium leading-5 text-gray-700">Jenis Kelamin</label>
+          <div class="mt-1 relative rounded-md shadow-sm">
+            <select v-model="gender" class="form-select block w-full" :class="{ 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red': errors.length > 0 }">
+              <option :value="null">
+                Pilih
+              </option>
+              <option value="M">
+                Laki-Laki
+              </option>
+              <option value="F">
+                Perempuan
+              </option>
+            </select>
+            <div v-if="errors.length > 0" class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+              <svg class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+              </svg>
+            </div>
+          </div>
+          <p class="mt-2 text-sm text-red-600">
+            {{ errors[0] }}
+          </p>
+        </validation-provider>
+      </div>
+
+      <div class="mt-6">
         <label class="block text-sm font-medium leading-5 text-gray-700">Tanggal Lahir</label>
         <validation-provider v-slot="{ errors }" name="Tanggal" rules="required">
           <div class="mt-1 relative rounded-md shadow-sm">
@@ -287,6 +314,15 @@ export default {
       },
       set (value) {
         this.$store.commit('form/SET_NAME', value)
+      }
+    },
+
+    gender: {
+      get () {
+        return this.$store.state.form.gender
+      },
+      set (value) {
+        this.$store.commit('form/SET_GENDER', value)
       }
     },
 

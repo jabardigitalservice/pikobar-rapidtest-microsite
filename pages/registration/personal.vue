a261ddb8c6e0adfac4cc00fa1f1824afba96cf13
Yoga Hanggara <yohang88@gmail.com>
2020-05-12 19:14:06 +0700

    phone number
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- pages/registration/personal.vue
+++ pages/registration/personal.vue
@@ -180,11 +180,13 @@
       <div class="mt-4">
         <validation-provider v-slot="{ errors }" name="Nomor Telepon" rules="required|phone_number">
           <label class="block text-sm font-medium leading-5 text-gray-700">Nomor Telepon (Whatsapp)</label>
-          <div class="mt-1 flex rounded-md shadow-sm">
-            <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500" :class="{ 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red': errors.length > 0 }">
-              +62
-            </span>
-            <input v-model="phone_number" class="form-input flex-1 block w-full px-3 py-2 rounded-none rounded-r-md" :class="{ 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red': errors.length > 0 }">
+          <div class="mt-1 relative rounded-md shadow-sm">
+            <input v-model.lazy="phone_number" type="text" class="form-input block w-full" :class="{ 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red': errors.length > 0 }">
+            <div v-if="errors.length > 0" class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+              <svg class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+              </svg>
+            </div>
           </div>
           <p class="mt-2 text-sm text-red-600">
             {{ errors[0] }}
@@ -230,7 +232,7 @@ import _ from 'lodash'
 
 extend('required', {
   ...required,
-  message: messages.requ
+  message: messages.required
 })
 
 extend('email', {
@@ -240,7 +242,7 @@ extend('email', {
 
 extend('phone_number', {
   validate: (value) => {
-    return value.match(/^8{1}[0-9]{9,11}$/g)
+    return value.match(/^0{1}8{1}[0-9]{9,11}$/g)
   },
   message: 'Format Nomor Telepon tidak benar'
 })

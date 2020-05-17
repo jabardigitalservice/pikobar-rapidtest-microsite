01055631010b1be5d213d119f923a6d106b6ff77
Yoga Hanggara <yohang88@gmail.com>
2020-05-17 22:30:29 +0700

    check status form
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- pages/check_registration/index.vue
+++ pages/check_registration/index.vue
@@ -4,28 +4,30 @@
       Cek Pendaftaran &amp; Hasil
     </h2>
 
-    <div class="mt-4">
-      <label class="block text-sm font-medium leading-5 text-gray-700">Nomor Pendaftaran</label>
-      <div class="mt-1 relative rounded-md shadow-sm">
-        <input :value="registration_code" type="text" class="form-input block w-full" @input="updateRegistrationCode($event.target.value)">
+    <form @submit.prevent="onSubmit">
+      <div class="mt-4">
+        <label class="block text-sm font-medium leading-5 text-gray-700">Nomor Pendaftaran</label>
+        <div class="mt-1 relative rounded-md shadow-sm">
+          <input :value="registration_code" type="text" class="form-input block w-full" @input="updateRegistrationCode($event.target.value)">
+        </div>
       </div>
-    </div>
 
-    <div class="mt-4">
-      <label class="block text-sm font-medium leading-5 text-gray-700">Berilah Tanda Centang</label>
-      <div class="mt-1">
-        <vue-recaptcha ref="recaptcha" :sitekey="recaptcha_key" :load-recaptcha-script="true" @verify="verifyCaptcha" />
+      <div class="mt-4">
+        <label class="block text-sm font-medium leading-5 text-gray-700">Berilah Tanda Centang</label>
+        <div class="mt-1">
+          <vue-recaptcha ref="recaptcha" :sitekey="recaptcha_key" :load-recaptcha-script="true" @verify="verifyCaptcha" />
+        </div>
       </div>
-    </div>
 
-    <div class="mt-12">
-      <button type="button" class="block w-full items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg text-white bg-brand-green-dark text-center" @click="submit">
-        Lanjutkan
-      </button>
-      <a href="https://api.whatsapp.com/send?phone=628112093306" target="_blank" class="block items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg border border-brand-green-dark text-brand-green-dark text-center mt-2">
-        Hubungi Pusat Bantuan PIKOBAR
-      </a>
-    </div>
+      <div class="mt-12">
+        <button type="submit" class="block w-full items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg text-white bg-brand-green-dark text-center">
+          Lanjutkan
+        </button>
+        <a href="https://api.whatsapp.com/send?phone=628112093306" target="_blank" class="block items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg border border-brand-green-dark text-brand-green-dark text-center mt-2">
+          Hubungi Pusat Bantuan PIKOBAR
+        </a>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -51,7 +53,7 @@ export default {
       this.registration_code = newValue.toUpperCase()
     },
 
-    async submit () {
+    async onSubmit () {
       try {
         const { data } = await this.$axios.$post('/api/rdt/check', {
           'g-recaptcha-response': this.recaptcha_response,

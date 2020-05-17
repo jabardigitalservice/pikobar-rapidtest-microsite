7495f575cc13c7471683eb41b82b941d1d64e117
Yoga Hanggara <yohang88@gmail.com>
2020-05-17 16:22:29 +0700

    caption result
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- pages/check_registration/result.vue
+++ pages/check_registration/result.vue
@@ -55,18 +55,29 @@
           {{ event.event_location }}
         </p>
 
-        <div class="bg-red-50 sm:rounded-lg border border-red-300 mt-8">
+        <div class="bg-gray-50 sm:rounded-lg border border-gray-300 mt-8">
           <div class="px-4 py-5 sm:p-6">
-            <h3 class="leading-6 font-bold text-red-900">
+            <h3 class="leading-6 font-bold text-gray-900">
               Informasi Penting
             </h3>
-            <div class="mt-2 max-w-xl text-sm leading-5 text-red-500">
-              <p>
-                Peserta wajib membawa fotokopi identitas (KTP) atau Kartu Keluarga (KK).
-              </p>
-              <p class="mt-4">
-                Peserta wajib menggunakan masker serta mengikuti protokol kesehatan perlidungan diri dan orang lain.
-              </p>
+            <div class="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+              <ol style="list-style: decimal" class="ml-4">
+                <li>
+                  Silahkan hadir pada jadwal waktu dan tempat yang sudah ditentukan.
+                </li>
+                <li class="mt-2">
+                  Tunjukkan undangan kode elektronik ini pada petugas checkin kehadiran saat tiba di lokasi.
+                </li>
+                <li class="mt-2">
+                  Wajib membawa fotokopi identitas (KTP) atau Kartu Keluarga (KK).
+                </li>
+                <li class="mt-2">
+                  Wajib menggunakan masker serta mengikuti protokol kesehatan perlidungan diri dan orang lain.
+                </li>
+                <li class="mt-2">
+                  Informasi dan pertanyaan lebih lanjut hubungi Hotline Dinas Kesehatan Provinsi Jawa Barat: <a href="tel:628112093306" class="font-bold">08112093306</a> atau Dinas Kesehatan Kota/Kabupaten setempat.
+                </li>
+              </ol>
             </div>
           </div>
         </div>

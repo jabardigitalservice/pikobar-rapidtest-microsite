72faaab7d2cc412c70ce107aba8121d14f44885f
Yoga Hanggara <yohang88@gmail.com>
2020-06-06 12:50:25 +0700

    confirm questions
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- pages/confirmation/hello.vue
+++ pages/confirmation/hello.vue
@@ -6,18 +6,25 @@
       Hi, Ahmad Yurianto
     </h1>
     <p class="mt-4 text-sm">
-      Selamat Datang di halaman konfirmasi undangan Tes Masif COVID-19 PIKOBAR Jawa Barat. Untuk melengkapi data anda kami ingin menanyakan beberapa pertanyaan berikut.
-    </p>
-    <p class="mt-4 font-bold">
-      Apakah Anda masih ingin mengikuti Tes COVID-19?
+      Selamat Datang di halaman konfirmasi undangan Tes Masif COVID-19 PIKOBAR Jawa Barat. Untuk melengkapi data Anda kami ingin menanyakan beberapa pertanyaan berikut.
     </p>
     <div class="mt-6">
-      <button type="button" class="block w-full items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg text-white bg-brand-green-dark text-center">
-        Ya, ingin mengikuti
+      <button type="button" class="block w-full items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg text-white bg-brand-green-dark text-center" @click="nextStep">
+        Lanjutkan
       </button>
-      <button type="button" class="block w-full items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg border border-red-700 text-red-700 text-center mt-2">
-        Tidak ingin mengikuti
-      </button>
+      <nuxt-link to="/confirmation" class="block items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg border border-brand-green-dark text-brand-green-dark text-center mt-2">
+        Kembali
+      </nuxt-link>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    nextStep () {
+      return this.$router.push('/confirmation/q_invitation')
+    }
+  }
+}
+</script>
---
+++ pages/confirmation/q_invitation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container mx-auto p-6">
+    <p>
+      Apakah Anda pernah mendapatkan undangan mengikuti Tes Masif COVID-19 dari PIKOBAR?
+    </p>
+
+    <div class="mt-24">
+      <button type="button" class="block w-full items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg border border-brand-green-dark text-brand-green-dark hover:bg-brand-green-dark hover:text-white text-center" @click="nextStep">
+        Ya
+      </button>
+      <button type="button" class="block w-full items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg border border-brand-green-dark text-brand-green-dark hover:bg-brand-green-dark hover:text-white text-center mt-2">
+        Tidak
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    nextStep () {
+      return this.$router.push('/confirmation/q_attend')
+    }
+  }
+}
+</script>

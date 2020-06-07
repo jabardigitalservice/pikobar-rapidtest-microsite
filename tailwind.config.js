4b73cd1f38163df8d0251cb65c20af6a065b8be3
Yoga Hanggara <yohang88@gmail.com>
2020-06-07 23:28:25 +0700

    tailwind purge
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
+  purge: [
+    './pages/**/*.html',
+    './pages/**/*.vue',
+    './pages/**/*.jsx'
+  ],
   theme: {
     extend: {
       colors: {

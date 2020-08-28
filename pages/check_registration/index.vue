7ea47d66fd08309f6ae06c05c6e359ffff2e7069
Yoga Hanggara <yohang88@gmail.com>
2020-08-28 19:34:52 +0700

    loader
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- pages/check_registration/index.vue
+++ pages/check_registration/index.vue
@@ -20,7 +20,18 @@
       </div>
 
       <div class="mt-12">
-        <button type="submit" class="block w-full items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg text-white bg-brand-green-dark text-center">
+        <button type="submit" class="w-full flex items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg text-white bg-brand-green-dark text-center" :disabled="loading">
+          <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            />
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+          </svg>
           Lanjutkan
         </button>
         <button-link-call-center />
@@ -44,7 +55,8 @@ export default {
     return {
       recaptcha_key: process.env.googleRecaptchaKey,
       recaptcha_response: null,
-      registration_code: null
+      registration_code: null,
+      loading: false
     }
   },
 
@@ -54,6 +66,8 @@ export default {
     },
 
     async onSubmit () {
+      this.loading = true
+
       try {
         const { data } = await this.$axios.$post('/api/rdt/check', {
           'g-recaptcha-response': this.recaptcha_response,
@@ -101,6 +115,7 @@ export default {
       } finally {
         this.recaptcha_response = null
         this.$refs.recaptcha.reset()
+        this.loading = false
       }
     },
 

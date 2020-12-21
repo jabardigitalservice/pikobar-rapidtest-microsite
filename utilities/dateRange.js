44775e4201251edc0d31db53b35bc3a7dfa1f144
maruf <maruf.harsono@gmail.com>
2020-12-21 10:02:45 +0700

    Kloter: added timeEnd

--- utilities/dateRange.js
+++ utilities/dateRange.js
@@ -25,9 +25,10 @@ export function getSchedule (lastInvitation, type) {
   const scheduleStartString = format(utcToZonedTime(scheduleStart, process.env.localTimezone), 'eeee, dd MMMM yyyy', { locale: id })
   const scheduleEndAtString = format(utcToZonedTime(scheduleEnd, process.env.localTimezone), 'eeee, dd MMMM yyyy', { locale: id })
   const timeDataStart = format(utcToZonedTime(scheduleStart, process.env.localTimezone), 'HH:mm', { locale: id })
+  const timeDataEnd = format(utcToZonedTime(scheduleEnd, process.env.localTimezone), 'HH:mm', { locale: id })
 
   return `${scheduleStartString} <br />
   s/d <br />
   ${scheduleEndAtString} <br />
-  ${timeDataStart}`
+  ${timeDataStart} - ${timeDataEnd}`
 }

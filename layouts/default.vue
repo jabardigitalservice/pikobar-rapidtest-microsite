ad2ccf2db2fddb28bbac7995c9fc6a36208c7618
maruf <maruf.harsono@gmail.com>
2021-01-20 16:47:16 +0700

    RedesignHomePage: update logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <div class="w-full bg-white leading-6 font-medium p-3 text-center sticky top-0 z-20">
       <nuxt-link to="/">
         <span>
-          <img src="/pikobar-header2.png" class="m-auto" width="350px" height="75px">
+          <img src="/pikobar-header2.png" class="m-auto" width="300px" height="80px">
         </span>
       </nuxt-link>
     </div>

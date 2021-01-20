90e8d4e71d192e01060775c2c65759bc7ebc3c70
maruf <maruf.harsono@gmail.com>
2021-01-20 15:26:33 +0700

    RedesignHomePage: redesign home

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,83 @@
 <template>
   <div class="container mx-auto">
     <div class="mobile-screen relative w-full p-6">
-      <div class="mobile-content">
-        <logo />
+      <div class="mobile-content mt-12">
+        <template v-if="enableRegistration === false">
+          <div>
+            <h1 class="text-xl font-bold text-brand-grey">
+              Selamat Datang,
+            </h1>
+            <p class="text-base mt-4 text-brand-grey">
+              Anda berada di halaman Pikobar Tes Masif COVID-19 Jawa Barat. Pendaftaran dapat dilakukan melalui <strong>Aplikasi PIKOBAR</strong> pada menu <strong>Periksa Mandiri</strong>.
+            </p>
+            <p class="mt-4">
+              Unduh melalui <a href="https://play.google.com/store/apps/details?id=id.go.jabarprov.pikobar" class="font-bold underline" target="_blank">Google Play</a> atau <a href="https://apps.apple.com/us/app/pikobar-jawa-barat/id1509760172" class="font-bold underline" target="_blank">Apple Store</a>.
+            </p>
+          </div>
+        </template>
+        <template v-if="enableRegistration">
+          <div v-if="event !== null">
+            <h1 class="text-xl font-bold text-brand-grey">
+              Selamat Datang,
+            </h1>
+            <p v-if="event.is_ended" class="text-base mt-4 text-brand-grey">
+              Anda berada di halaman Pikobar Tes Masif COVID-19 Jawa Barat. Mohon maaf Kegiatan <span class="font-bold text-red-700"><strong>{{ event.event_name }}.</strong> Sudah Selesai.</span>
+            </p>
+            <p v-else class="text-base mt-4 text-brand-grey">
+              Anda berada di halaman Pikobar Tes Masif COVID-19 Jawa Barat. Anda dapat mengikuti kegiatan <strong>{{ event.event_name }}.</strong> Silahkan pilih menu <strong>Pendaftaran Baru</strong> di bawah, untuk melengkapi data-data Anda.
+            </p>
+          </div>
+        </template>
 
         <div class="mt-8">
-          <template v-if="enableRegistration === false">
-            <div class="bg-gray-50 sm:rounded-lg border border-gray-300">
-              <div class="px-4 py-5 sm:p-6">
-                <h3 class="leading-6 font-bold text-gray-900">
-                  Pendaftaran Baru
-                </h3>
-                <div class="mt-2 max-w-xl text-sm leading-5 text-gray-800">
-                  <p>
-                    Pendaftaran dapat dilakukan melalui <strong>Aplikasi PIKOBAR</strong> pada menu <strong>Periksa Mandiri</strong>.
-                  </p>
-                  <p class="mt-4">
-                    Unduh melalui <a href="https://play.google.com/store/apps/details?id=id.go.jabarprov.pikobar" class="font-bold underline" target="_blank">Google Play</a> atau <a href="https://apps.apple.com/us/app/pikobar-jawa-barat/id1509760172" class="font-bold underline" target="_blank">Apple Store</a>.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </template>
+          <h1 class="text-xl font-bold text-brand-green">
+            Apa yang ingin Anda
+          </h1>
+          <h1 class="text-xl font-bold text-brand-green">
+            Lakukan?
+          </h1>
         </div>
 
         <div>
-          <template v-if="enableRegistration">
-            <template v-if="event !== null">
-              <div class="bg-gray-50 sm:rounded-lg border border-gray-300 mb-8">
-                <div class="px-4 py-5 sm:p-6">
-                  <div class="mt-2 max-w-xl text-sm leading-5 text-gray-800">
-                    <p class="mb-2">
-                      <strong>Pendaftaran Peserta</strong>
-                    </p>
-                    <p>
-                      {{ event.event_name }}
-                    </p>
-                    <p v-if="event.is_ended" class="mt-4 font-bold text-red-700">
-                      Kegiatan Tes Masif Sudah Selesai
-                    </p>
-                  </div>
+          <template v-if="enableRegistration && !(event && event.is_ended)">
+            <nuxt-link to="/terms-conditions">
+              <div class="flex items-center sm:rounded-lg border-2 border-brand-green my-4">
+                <div class="flex-1 text-grey-darker text-center px-4 py-2 m-2">
+                  <img src="/registration-icon2.png" alt="Cek-status" width="112px" height="96">
+                </div>
+                <div class="flex-1 text-grey-darker text-left bg-grey-light px-4 py-2 m-2">
+                  <p class="text-lg font-bold text-brand-green">
+                    Pendaftaran Baru
+                  </p>
                 </div>
               </div>
-            </template>
-
-            <nuxt-link to="/terms-conditions" class="block items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg text-white bg-brand-green-dark text-center">
-              Pendaftaran Baru
             </nuxt-link>
           </template>
 
-          <nuxt-link to="/check_registration" class="block items-center justify-center px-5 py-3 text-base leading-6 font-medium rounded-lg border border-brand-green-dark text-center mt-2" :class="{ 'bg-brand-green-dark text-white': enableRegistration === false, 'text-brand-green-dark': enableRegistration === true }">
-            Cek Pendaftaran &amp; Hasil
+          <nuxt-link to="/check_registration">
+            <div class="flex items-center sm:rounded-lg border-2 border-brand-green my-4">
+              <div class="flex-1 text-grey-darker text-center px-4 py-2 m-2">
+                <img src="/check-status-icon2.png" alt="Cek-status" width="112px" height="96">
+              </div>
+              <div class="flex-1 text-grey-darker text-left bg-grey-light px-4 py-2 m-2">
+                <p class="text-lg font-bold text-brand-green">
+                  Cek Status &amp; Hasil Test
+                </p>
+              </div>
+            </div>
           </nuxt-link>
-          <button-link-call-center />
+          <a href="https://s.id/HotlinePikobar" target="_blank">
+            <div class="flex items-center sm:rounded-lg border-2 border-brand-green my-4">
+              <div class="flex-1 text-grey-darker text-center px-4 py-2 m-2">
+                <img src="/call-center-icon2.png" alt="Cek-status" width="112px" height="96">
+              </div>
+              <div class="flex-1 text-grey-darker text-left bg-grey-light px-4 py-2 m-2">
+                <p class="text-lg font-bold text-brand-green">
+                  Hubungi Pusat Bantuan PIKOBAR
+                </p>
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     </div>
@@ -63,15 +88,8 @@
 import { isAfter } from 'date-fns'
 import { mapGetters } from 'vuex'
 import Cookies from 'js-cookie'
-import Logo from '@/components/Logo.vue'
-import ButtonLinkCallCenter from '@/components/ButtonLinkCallCenter'
 
 export default {
-  components: {
-    Logo,
-    ButtonLinkCallCenter
-  },
-
   data () {
     return {
       event: null

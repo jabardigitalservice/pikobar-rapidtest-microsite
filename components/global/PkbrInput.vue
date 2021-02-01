7b03d00a5fda28a9b9e5e0542112b0bad44f63f6
maruf <maruf.harsono@gmail.com>
2021-02-01 22:57:53 +0700

    RedesignForm: added customize style for form

--- components/global/PkbrInput.vue
+++ components/global/PkbrInput.vue
@@ -2,11 +2,11 @@
   <validation-provider v-slot="{ errors }" :name="name" :rules="rules" tag="div" class="block">
     <label :for="name" class="block text-gray-700 leading-5 text-sm font-medium">
       {{ label }}
-      <span v-if="isRequired" class="text-red-500">*</span>
+      <span v-if="isRequired" class="text-brand-red">*</span>
     </label>
     <div class="mt-1 relative rounded-md">
       <div v-if="errors.length > 0" class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-        <svg class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+        <svg class="h-5 w-5 text-brand-red" fill="currentColor" viewBox="0 0 20 20">
           <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
         </svg>
       </div>
@@ -32,9 +32,9 @@
         @keypress="isNumberTel"
       >
     </div>
-    <strong class="text-red-500 text-xs italic">
+    <p class="text-brand-red text-sm">
       {{ errors[0] }}
-    </strong>
+    </p>
   </validation-provider>
 </template>
 
--- components/global/PkbrSelect.vue
+++ components/global/PkbrSelect.vue
@@ -2,7 +2,7 @@
   <validation-provider v-slot="{ errors }" :name="name" :rules="rules" tag="div" class="block">
     <label class="block text-sm font-medium leading-5 text-gray-700">
       {{ label }}
-      <span v-if="isRequired" class="text-red-500">*</span>
+      <span v-if="isRequired" class="text-brand-red">*</span>
     </label>
     <div class="mt-1 relative rounded-md">
       <select
@@ -21,14 +21,14 @@
         </option>
       </select>
       <div v-if="errors.length > 0" class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-        <svg class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+        <svg class="h-5 w-5 text-brand-red" fill="currentColor" viewBox="0 0 20 20">
           <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
         </svg>
       </div>
     </div>
-    <strong class="mt-2 text-xs text-red-500 italic">
+    <p class="mt-2 text-sm text-brand-red">
       {{ errors[0] }}
-    </strong>
+    </p>
   </validation-provider>
 </template>
 

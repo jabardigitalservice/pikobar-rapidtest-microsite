c934afa47d8ce8afb2cf73a0c27ee456d10973ac
maruf <maruf.harsono@gmail.com>
2021-07-01 14:12:20 +0700

    fixingSentry: setup sentry performance

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,6 +76,16 @@ export default {
     config: {
       environment: process.env.APP_ENV || 'local'
     },
+    tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE,
+    vueOptions: {
+      tracing: true,
+      tracingOptions: {
+        hooks: ['mount', 'update'],
+        timeout: 2000,
+        trackComponents: true
+      }
+    },
+    browserOptions: {},
     beforeSend (event, hint) {
       return (hint.originalException === 'Timeout') ? null : event
     }
